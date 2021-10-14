d6bab0029c94ea2a554ce9eacbeae09ebd6fcfd9
hongjun95 <hongjunjeong95@gmail.com>
2021-10-14 12:03:55 +0900

    Amend home page

---
+++ src/common/react-query/query-keys.ts
@@ -0,0 +1,78 @@
+// Like Keys
+
+import { SortState } from '@interfaces/item.interface';
+
+export const likeKeys = {
+  all: ['likeLists'] as const,
+  details: () => [...likeKeys.all, 'detail'] as const,
+  detail: (id: string) => [...likeKeys.details(), id] as const,
+};
+
+// Product Keys
+
+export const itemKeys = {
+  all: ['items'] as const,
+  lists: () => [...itemKeys.all, 'list'] as const,
+  list: ({ sort, categoryId }: { sort: string; categoryId: string }) =>
+    [...itemKeys.lists(), sort, categoryId] as const,
+  details: () => [...itemKeys.all, 'detail'] as const,
+  detail: (id: string) => [...itemKeys.details(), id] as const,
+  search: ({ sort, page, query }: { sort: SortState; page?: number; query: string }) =>
+    [...itemKeys.lists(), sort, page, query] as const,
+};
+
+export const itemsFromProviderKeys = {
+  all: ['items', 'provider'] as const,
+  lists: () => [...itemsFromProviderKeys.all, 'list'] as const,
+  list: ({ sort }: { sort: string }) => [...itemsFromProviderKeys.lists(), sort] as const,
+  details: () => [...itemsFromProviderKeys.all, 'detail'] as const,
+  detail: (id: string) => [...itemsFromProviderKeys.details(), id] as const,
+};
+
+// Order Keys
+
+export const ordersFromConsumer = {
+  all: ['orders', 'consumer'] as const,
+  lists: () => [...ordersFromConsumer.all, 'list'] as const,
+  list: ({ consumerId }: { consumerId: string }) => [...ordersFromConsumer.lists(), consumerId] as const,
+  details: () => [...ordersFromConsumer.all, 'detail'] as const,
+  detail: (orderId: string) => [...ordersFromConsumer.details(), orderId] as const,
+};
+
+export const ordersFromProvider = {
+  all: ['orders', 'provider'] as const,
+  lists: () => [...ordersFromProvider.all, 'list'] as const,
+  list: ({ providerId }: { providerId: string }) => [...ordersFromProvider.lists(), providerId] as const,
+  details: () => [...ordersFromProvider.all, 'detail'] as const,
+  detail: (orderId: string) => [...ordersFromProvider.details(), orderId] as const,
+};
+
+// Refund Keys
+
+export const refundsFromConsumer = {
+  all: ['refunds', 'consumer'] as const,
+  lists: () => [...refundsFromConsumer.all, 'list'] as const,
+  list: ({ page, consumerId }: { page?: number; consumerId: string }) =>
+    [...refundsFromConsumer.lists(), consumerId, page] as const,
+  details: () => [...refundsFromConsumer.all, 'detail'] as const,
+  detail: (orderId: string) => [...refundsFromConsumer.details(), orderId] as const,
+};
+
+export const refundsFromProvider = {
+  all: ['refunds', 'provider'] as const,
+  lists: () => [...refundsFromProvider.all, 'list'] as const,
+  list: ({ page, providerId }: { page?: number; providerId: string }) =>
+    [...refundsFromProvider.lists(), providerId, page] as const,
+  details: () => [...refundsFromProvider.all, 'detail'] as const,
+  detail: (orderId: string) => [...refundsFromProvider.details(), orderId] as const,
+};
+
+// Review Keys
+
+export const reviewKeys = {
+  all: ['reviews'] as const,
+  lists: () => [...reviewKeys.all, 'list'] as const,
+  list: ({ page, itemId }: { page?: number; itemId: string }) => [...reviewKeys.lists(), itemId, page] as const,
+  details: () => [...reviewKeys.all, 'detail'] as const,
+  detail: (id: string) => [...reviewKeys.details(), id] as const,
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,122 @@
-import Categories from '@components/Categories';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link, Navbar, NavLeft, NavRight, NavTitle, Page } from 'framework7-react';
-import React from 'react';
+import { useRecoilState } from 'recoil';
+import { useInfiniteQuery, useQueryClient } from 'react-query';
+import { useInView } from 'react-intersection-observer';
+import { useFormik } from 'formik';
 
-const HomePage = () => (
-  <Page name="home">
-    <Navbar>
-      <NavLeft>
-        <Link icon="las la-bars" panelOpen="left" />
-      </NavLeft>
-      <NavTitle>인썸니아</NavTitle>
-      <NavRight>
-        <Link href="/line_items" iconF7="cart" iconBadge={3} badgeColor="red" />
-      </NavRight>
-    </Navbar>
-    <Categories />
-  </Page>
-);
+import { shoppingListAtom } from '@atoms';
+import Categories from '@components/Categories';
+import useAuth from '@hooks/useAuth';
+import { getShoppingList, IShoppingItem } from '@store';
+import SearchBar from '@components/SearchBar';
+import ItemsList from '@components/ItemsList';
+import { GetItemsBySearchTermOutput, SortState } from '@interfaces/item.interface';
+import { itemKeys } from '@reactQuery/query-keys';
+import { getItemsBySearchTermAPI } from '@api';
+
+interface ItemFilterProps {
+  sort: SortState;
+}
+
+const HomePage = ({ f7router }) => {
+  const { currentUser } = useAuth();
+  const [shoppingList, setShoppingList] = useRecoilState<Array<IShoppingItem>>(shoppingListAtom);
+  const [query, setQuery] = useState<string>('');
+  const [page, setPage] = useState<number>(1);
+  const { ref, inView, entry } = useInView({
+    threshold: 0,
+  });
+
+  const queryClient = useQueryClient();
+
+  const filterForm = useFormik<ItemFilterProps>({
+    initialValues: {
+      sort: 'createdAt desc',
+    },
+
+    onSubmit: async () => {
+      await queryClient.removeQueries(ITEM_KEY);
+      await refetch();
+    },
+  });
+
+  const ITEM_KEY = itemKeys.search({ sort: filterForm.values.sort, page, query });
+
+  const {
+    hasNextPage, //
+    isFetchingNextPage,
+    isFetching,
+    data,
+    error,
+    status,
+    fetchNextPage,
+    refetch,
+  } = useInfiniteQuery<GetItemsBySearchTermOutput, Error>(
+    ITEM_KEY,
+    ({ pageParam }) => getItemsBySearchTermAPI({ sort: filterForm.values.sort, page: pageParam, query }),
+    {
+      getNextPageParam: (lastPage) => {
+        const hasNextPage = lastPage.hasNextPage;
+        return hasNextPage ? lastPage.nextPage : false;
+      },
+      onSuccess: (data) => {
+        if (hasNextPage) {
+          setPage(data.pages[0].nextPage);
+        }
+      },
+      enabled: !!query,
+    },
+  );
+
+  const itemsRefetch = useCallback(() => {
+    refetch();
+  }, []);
+
+  const useSetQuery = useCallback((query) => {
+    setQuery(query);
+  }, []);
+
+  useEffect(() => {
+    setShoppingList(getShoppingList(currentUser?.id));
+    if (inView && hasNextPage && !isFetching && entry.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetching, currentUser?.id]);
+
+  const onRefresh = async (done) => {
+    await queryClient.removeQueries(ITEM_KEY);
+    await itemsRefetch();
+    done();
+  };
+
+  return (
+    <Page name="home" onPtrRefresh={onRefresh} ptr>
+      <Navbar>
+        <NavLeft>
+          <Link icon="las la-bars" panelOpen="left" />
+        </NavLeft>
+        <NavTitle>Houpang</NavTitle>
+        <NavRight>
+          <Link href="/shopping-list" iconF7="cart" iconBadge={shoppingList.length} badgeColor="red" />
+        </NavRight>
+      </Navbar>
+      <SearchBar query={query} setQuery={useSetQuery} refetch={itemsRefetch} />
+      <Categories />
+      <ItemsList //
+        f7router={f7router}
+        status={status}
+        error={error}
+        data={data}
+        ITEM_KEY={ITEM_KEY}
+        filterForm={filterForm}
+      />
+      <div className="flex justify-center font-bold mt-4">
+        <div ref={hasNextPage && !isFetching ? ref : null}>
+          {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load Newer' : ''}
+        </div>
+      </div>
+    </Page>
+  );
+};
 export default React.memo(HomePage);
